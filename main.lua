44e8022b2d54db742ecb14c963b584a613409d55
h9458666231 <44384614+h9458666231@users.noreply.github.com>
2018-10-30 08:53:19 +0800

    02

--- main.lua
+++ main.lua
@@ -4,8 +4,9 @@
 -- Sample code is MIT licensed; see https://www.coronalabs.com/links/code/license
 ---------------------------------------------------------------------------------------
 
-local msgText = display.newText( "Hello, world!", world.x, world.y+100, native.systemFont, 32 )
-msgText:setFillColor( 0.2, 0.6, 0.8 )
+local world = display.newImageRect( "world.png", 250, 250 )
+world.x = display.contentCenterX
+world.y = display.contentCenterY - 30
 
 local msgText = display.newText( "Hello, world!", world.x, world.y+160, native.systemFont, 32 )
 msgText:setFillColor( 0.2, 0.6, 0.8 )
